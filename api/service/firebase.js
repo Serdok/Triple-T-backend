54c82d528974d5af6e11d3afbcd12b6da2c37ac3
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2020-08-02 13:05:39 +0200

    :recycle: Added credential service worker info

--- api/service/firebase.js
+++ api/service/firebase.js
@@ -1,14 +1,6 @@
 const functions = require('firebase-functions');
-const config = {
-    apiKey: "AIzaSyA4-iPqaaVNzsyP_S7RzOvtHMtTVdCrNnw",
-    authDomain: "triple-t-e2e24.firebaseapp.com",
-    databaseURL: "https://triple-t-e2e24.firebaseio.com",
-    projectId: "triple-t-e2e24",
-    storageBucket: "triple-t-e2e24.appspot.com",
-    messagingSenderId: "133026401644",
-    appId: "1:133026401644:web:fb19e1cb86f73aaeb97749",
-    measurementId: "G-TN3STENB4J"
-};
+const config = functions.config().triple_t;
+const credential = functions.config().credential;
 
 const isDev = process.env.FUNCTIONS_EMULATOR;
 let app;
@@ -17,21 +9,16 @@ let db;
 if (isDev) {
     // Setup the emulator
     const firebase = require('@firebase/testing');
-    app = firebase.initializeTestApp({
+    app = firebase.initializeAdminApp({
         projectId: config.projectId,
     });
     db = app.firestore();
 } else {
     const admin = require('firebase-admin');
     app = admin.initializeApp({
-        apiKey: config.apiKey,
-        authDomain: config.authDomain,
-        databaseURL: config.databaseURL,
-        projectId: config.projectId,
-        storageBucket: config.storageBucket,
-        messagingSenderId: config.messagingSenderId,
-        appId: config.appId,
-        measurementId: config.measurementId,
+        ...config,
+
+        credential: admin.credential.cert(credential),
     });
     db = admin.firestore(app);
 }
